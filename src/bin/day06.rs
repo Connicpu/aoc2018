8b8503a75cc7baec80639ecc6c50284bcf323ee9
Connie Hilarides <conni_h@outlook.com>
2018-12-05 23:03:45 -0800

    Refactor 6 to remove smells

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -3,15 +3,27 @@ use std::collections::{HashMap, HashSet};
 use std::i32::{MAX, MIN};
 use std::str::FromStr;
 
+use math2d::Recti;
+
 static INPUT: &str = include_str!("day06.txt");
 
-type Coord = (i32, i32);
+#[derive(Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+impl Coord {
+    fn new(x: i32, y: i32) -> Self {
+        Coord { x, y }
+    }
+}
 
 fn parse_coord(input: &str) -> Option<Coord> {
     let mut split = input.split(',');
     let x = i32::from_str(split.next()?.trim()).ok()?;
     let y = i32::from_str(split.next()?.trim()).ok()?;
-    Some((x, y))
+    Some(Coord::new(x, y))
 }
 
 fn coords() -> impl Iterator<Item = Coord> {
@@ -19,13 +31,11 @@ fn coords() -> impl Iterator<Item = Coord> {
 }
 
 fn manhattan(a: Coord, b: Coord) -> i32 {
-    (a.0 - b.0).abs() + (a.1 - b.1).abs()
+    (a.x - b.x).abs() + (a.y - b.y).abs()
 }
 
-const BAD_COORD: Coord = (MIN / 8, MIN / 8);
-
-fn closest_coord(pos: Coord) -> Coord {
-    let mut closest = BAD_COORD;
+fn closest_coord(pos: Coord) -> Option<Coord> {
+    let mut closest = Coord::new(0, 0);
     let mut closest_dist = manhattan(closest, pos);
     let mut closest_count = 0;
     for c in coords() {
@@ -40,22 +50,21 @@ fn closest_coord(pos: Coord) -> Coord {
     }
 
     if closest_count > 1 {
-        BAD_COORD
+        None
     } else {
-        closest
+        Some(closest)
     }
 }
 
-fn bounds() -> (Coord, Coord) {
-    let mut tl = (MAX, MAX);
-    let mut br = (MIN, MIN);
+fn bounds() -> Recti {
+    let mut rect = Recti::new(MAX, MAX, MIN, MIN);
     for c in coords() {
-        tl.0 = min(tl.0, c.0);
-        tl.1 = min(tl.1, c.1);
-        br.0 = max(br.0, c.0);
-        br.1 = max(br.1, c.1);
+        rect.left = min(rect.left, c.x);
+        rect.top = min(rect.top, c.y);
+        rect.right = max(rect.right, c.x);
+        rect.bottom = max(rect.bottom, c.y);
     }
-    (tl, br)
+    rect
 }
 
 fn total_dist(pos: Coord) -> i32 {
@@ -63,15 +72,24 @@ fn total_dist(pos: Coord) -> i32 {
 }
 
 fn main() {
-    let ((left, top), (right, bottom)) = bounds();
+    let Recti {
+        left,
+        top,
+        right,
+        bottom,
+    } = bounds();
+
     let mut counts = HashMap::<Coord, usize>::new();
     let mut inf_blacklist = HashSet::new();
+
     for y in top - 1..=bottom + 1 {
         for x in left - 1..=right + 1 {
-            let id = closest_coord((x, y));
-            *counts.entry(id).or_default() += 1;
-            if y == top - 1 || y == bottom + 1 || x == left - 1 || x == right + 1 {
-                inf_blacklist.insert(id);
+            let c = Coord::new(x, y);
+            if let Some(id) = closest_coord(c) {
+                *counts.entry(id).or_default() += 1;
+                if y == top - 1 || y == bottom + 1 || x == left - 1 || x == right + 1 {
+                    inf_blacklist.insert(id);
+                }
             }
         }
     }
@@ -85,7 +103,7 @@ fn main() {
     let mut viable = 0;
     for y in top..=bottom {
         for x in left..=right {
-            let c = (x, y);
+            let c = Coord::new(x, y);
             if total_dist(c) < 10_000 {
                 viable += 1;
             }
